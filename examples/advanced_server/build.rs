1bdfdcc128bebe1985063bc9feeac660d83d96aa
Alex Unigovsky <unik@devrandom.ru>
2025-09-23 06:05:04 +0300

    chore: bump versions of tonic/prost

--- examples/advanced_server/build.rs
+++ examples/advanced_server/build.rs
@@ -2,7 +2,7 @@ use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(true)
         .build_client(false)
         .file_descriptor_set_path(out_dir.join("advanced_server.bin"))
